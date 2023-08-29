8112e245ebc7d66f73e7b5025340b2704d289de1
Wazokrit <wazokrit@gmail.com>
2023-08-29 20:36:39 +0300

    hw

--- src/test/java/HW_leumi.java
+++ src/test/java/HW_leumi.java
@@ -6,13 +6,15 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
 
 
 public class HW_leumi {
 
     WebDriver wd;
 
-
+ 
     @BeforeMethod
     public void precondition(){
         wd = new ChromeDriver();
@@ -20,7 +22,7 @@ public class HW_leumi {
     }
 
     @Test
-    public void testCss() throws UnsupportedEncodingException {
+    public void testCss() {
 
         WebElement we =wd.findElement(By.cssSelector(".first"));
         String str=we.findElement(By.cssSelector("span")).getText();
